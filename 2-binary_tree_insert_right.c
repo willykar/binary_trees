93c7bd18171a73f5d2940d410a7dd528406c203a
willykar <wilsonnjenga799@gmail.com>
2024-02-28 08:36:46 -0800

    update

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,10 +10,10 @@
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node = NULL;
+
 	if (parent == NULL)
 		return (NULL);
-
-	binary_tree_t new_node;
 
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node != NULL)
