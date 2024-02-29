ca6224ccf0932700dafc41ea0b84f3c3207a1919
johnmuthua <caveomuthama@gmail.com>
2024-02-29 20:04:24 +0300

    Add 111

--- binary_trees.h
+++ binary_trees.h
@@ -38,6 +38,7 @@ typedef struct link_s
 } link_t;
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -64,5 +65,6 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_complete(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
 int binary_tree_is_avl(const binary_tree_t *tree);
 #endif
