026faa151bd02426b39102c578c96a9ba4f5245b
willykar <wilsonnjenga799@gmail.com>
2024-02-29 09:31:46 -0800

    update

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,20 +11,16 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	else
-	{
-		if (tree->left && tree->right)
-		{
-			left = binary_tree_is_full(tree->left);
-			right = binary_tree_is_full(tree->right);
-			if (left == 0 || right == 0)
-				return (0);
-			return (1);
-		}
-		else if (!tree->left && !tree->right)
-			return (1);
-		else
-			return (0);
-	}
-}
 
+	if ((tree->left == NULL) && (tree->right == NULL))
+		return (1);
+
+	left = binary_tree_is_full(tree->left);
+	right = binary_tree_is_full(tree->right);
+
+	if (left == 0 || right == 0)
+		return (0);
+
+	else
+		return (1);
+}
