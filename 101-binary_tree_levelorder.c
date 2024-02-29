5568744bdd00c5803fa16b22f0e4255303e28f22
willykar <wilsonnjenga799@gmail.com>
2024-02-29 13:13:04 -0800

    update

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -6,22 +6,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l = 0;
-	size_t r = 0;
+	size_t left_height = 0, right_height = 0;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
-	else
-	{
-		if (tree)
-		{
-			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		}
-		return ((l > r) ? l : r);
-	}
+
+	if (tree->left != NULL)
+		left_height = binary_tree_height(tree->left);
+	if (tree->right != NULL)
+		right_height = binary_tree_height(tree->right);
+
+	return (left_height >= right_height ?
+			(left_height + 1) : (right_height + 1));
 }
 /**
  * binary_tree_depth - depth of specified node from root
