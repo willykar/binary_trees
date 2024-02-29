f3248edc0515ced4e4fa0f17bac6d35b4e338e18
johnmuthua <caveomuthama@gmail.com>
2024-02-29 10:20:27 +0300

    add betty 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,11 +8,10 @@
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
 
-    int left_height = binary_tree_height(tree->left);
-    int right_height = binary_tree_height(tree->right);
-
-    return left_height - right_height;
+	return (left_height - right_height);
 }
